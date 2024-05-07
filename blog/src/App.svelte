ea49998f4eb89dcd7e74dad64820e549488992ba
syruponwaffles <nicholaskor@hotmail.com>
2024-05-06 22:28:21 -0600

    initial commit

---
+++ blog/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="js">
+  import Comments from "./lib/Comments.svelte";
+import Login from "./lib/Login.svelte";
+
+</script>
+
+<main>
+  <h1><u>syruponwaffles' portfolio</u></h1>
+  <h2><u>about me</u></h2>
+  <div><p>hello my name is nicholas korzeniowski and im a programmer from alberta canada.</p></div>
+  <div><p>i am familiar in many languages including c, python and javascript and have used game making tools such as godot, unity, pygame, raylib and gamemaker </p></div>
+  <h2><u>projects</u></h2>
+  <div><p>i have recently taken a strong interest in network programming, developing <a href="https://github.com/SyrupOnWaffles/raycaster">a simple game server</a> which syncs player positions across multiple clients, and <a href="https://github.com/SyrupOnWaffles/c-http-server">a web server written in pure c</a> which is what this website is running off right now!</p></div>
+  <div><p>an extension of this is a realtime chat app i made akin to discord called <a href="https://syrup.is-very.fun">bannock chat</a> built with vanilla js and flask python. to increase security, i ran this server through an nginx reverse proxy to deal with encryption, rate limiting etc. i plan to do a lot more web applications in the future as well.</p></div>
+  <div><p>in the past i have heavily delved into the world of software 3d renderering creating a model viewer in <a href="https://github.com/SyrupOnWaffles/Pygame-3D-Engine-WIP-">pygame</a> and in <a href="https://github.com/SyrupOnWaffles/terminalmodelviewer">the unix terminal</a> (this code is very bad and does not represent my current c knowledge). recently i have also delved in the world of psuedo 3d with a <a href="https://github.com/SyrupOnWaffles/raycaster">raycasting engine</a></p></div>
+  <h2><u>achievements</u></h2>
+  <div><a href="https://itch.io/jam/joystick-input-jam">winner of the analog joystick input jam #1</a></div>
+  <div><p>i know there were 3 submissions but thats gotta count for something right?</p></div>
+  <h2><u>links</u></h2>
+  <div><p><a href="https://github.com/SyrupOnWaffles/">github</a> <br><a href="https://syruponwaffles.itch.io/">itch.io</a> <br><a href="mailto:syruponwaffles@420blaze.it">email</a> </p></div>
+  <div><h3><i>thanks for reading</i></h3></div>
+  <hr>
+<!-- <div><img src="https://i.kym-cdn.com/entries/icons/original/000/018/929/graphic_design_is_my_passion.png"></div> -->
+</main>
+
+<h1>Comments</h1>
+<Login />
+<br>
+<hr class="half">
+<br>
+<Comments />
---
+++ blog/src/lib/Comments.svelte
@@ -0,0 +1,46 @@
+<script lang="js">
+    import {onMount, onDestroy} from 'svelte';
+    import {currentUser, pb} from './pocketbase'
+
+    let comments = [];
+    let newMessage;
+
+    onMount(async () =>{    
+        const resultList = await pb.collection('comments').getList(1,50, {
+            sort:'-created',
+            expand:'user',
+        });
+        comments = resultList.items;
+
+        pb 
+            .collection('comments')
+            .subscribe('*',async ({action,record})=>{
+                if(action === "create"){
+                    const user = await pb.collection('users').getOne(record.user);
+                    record.expand = {user};
+                    comments = [...comments, record]
+                }
+            });
+    });
+
+    async function sendMessage(){
+        const data = {
+            text: newMessage,
+            user: $currentUser.id,
+        };
+        const createdMessage = await pb.collection('comments').create(data);
+    }
+</script>
+
+<form on:submit={sendMessage}>
+    <input placeholder="comment" type="text" bind:value={newMessage}/>
+    <button type="Submit">send</button>
+</form> 
+
+<div class="comments">
+   {#each comments as comment (comment.id)}
+        <div class="comment">
+            <p class="comment-text">@{comment.user}: {comment.text}</p>
+        </div>
+   {/each} 
+</div>  
