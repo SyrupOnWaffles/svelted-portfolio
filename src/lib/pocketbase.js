bce76981576e2bdc686808237d60bbe51ca348d9
syruponwaffles <nicholaskor@hotmail.com>
2024-05-08 16:12:45 -0600

    db public

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase("https://portfoliobackend.duckdns.org/");
+export const pb = new PocketBase("https://portfoliobackend.mooo.com");
 
 export const currentUser = writable(pb.authStore.model)
 
