aea3d01863db325c56c6f9483d1bdf30f5486b0a
syruponwaffles <nicholaskor@hotmail.com>
2024-05-07 20:27:20 -0600

    fixed?

--- blog/src/lib/pocketbase.js
+++ blog/src/lib/pocketbase.js
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase("http://syrup.is-very.fun:8090");
+export const pb = new PocketBase("http://syrup.is-very.fun/api/");
 
 export const currentUser = writable(pb.authStore.model)
 
