a80c240549290fe546fd62e19bcc8370c1061b1e
syruponwaffles <nicholaskor@hotmail.com>
2024-05-07 20:10:25 -0600

    external pb

--- blog/src/lib/pocketbase.js
+++ blog/src/lib/pocketbase.js
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase("http://35.247.54.205:8090");
+export const pb = new PocketBase("http://syrup.is-very.fun:8090");
 
 export const currentUser = writable(pb.authStore.model)
 
